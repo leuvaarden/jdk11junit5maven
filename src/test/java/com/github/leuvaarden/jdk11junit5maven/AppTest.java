c01ab994d66ebf5f8c05f9bae6bcc7e2717721ea
leuvaarden <probablyviolin@gmail.com>
2021-12-09 13:54:25 +0300

    Init

---
+++ src/test/java/com/github/leuvaarden/jdk11junit5maven/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.leuvaarden.jdk11junit5maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
