c742ca94d328f6b806309e8d5e925347ae055444
leuvaarden <probablyviolin@gmail.com>
2021-12-09 14:25:27 +0300

    Fix

--- src/main/java/com/github/leuvaarden/jdk11junit5maven/App.java
+++ src/main/java/com/github/leuvaarden/jdk11junit5maven/App.java
@@ -3,18 +3,10 @@ package com.github.leuvaarden.jdk11junit5maven;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Clock;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        String result = OffsetDateTime.now(Clock.systemUTC())
-                .truncatedTo(ChronoUnit.SECONDS)
-                .format(DateTimeFormatter.ISO_DATE_TIME);
-        logger.info(result);
+        logger.info("Hello, world!");
     }
 }
